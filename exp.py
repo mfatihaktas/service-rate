fa5a59350f45b7ef5f93a6a7db00eb0051920cd2
olekol33 <olekol@gmail.com>
2022-05-07 13:51:10 +0300

    - Added input and output file interface Input: process files for EC and replication Output: Check if each vector is inside region and add to result file, Merge result files into a single file

--- exp.py
+++ exp.py
@@ -5,7 +5,8 @@ from csv_utils import (
 from debug_utils import *
 from service_rate import ServiceRateInspector
 from storage_scheme import Obj, StorageScheme, name_to_node_objs_list_map
-
+from pathlib import Path
+import pandas as pd
 
 def run(node_id_objs_list: list[list[Obj]]):
     log(DEBUG, "Started;", node_id_objs_list=node_id_objs_list)
@@ -53,13 +54,20 @@ def run_w_csv_file_path(
     obj_demands_list = get_obj_demands_list_from_oleg_csv_file(
         csv_file_path_for_obj_demands_list
     )
+    outfile = createResultFilePath(csv_file_path_for_obj_demands_list)
+    demDF = pd.read_csv(csv_file_path_for_obj_demands_list)
+    demDF["inside"] = 0
 
     num_is_in_cap_region = 0
     for i, obj_demand_list in enumerate(obj_demands_list):
         is_in_cap_region = inspector.is_in_cap_region(obj_demand_list)
         # log(DEBUG, f"demand-vector-{i}: is_in_cap_region= {is_in_cap_region}")
-
         num_is_in_cap_region += int(is_in_cap_region)
+        demDF.loc[i,"inside"] = is_in_cap_region
+
+    demDF.to_csv(outfile,index=False)
+
+
 
     log(
         DEBUG,
@@ -67,6 +75,11 @@ def run_w_csv_file_path(
         freq_is_in_cap_region=num_is_in_cap_region / len(obj_demands_list),
     )
 
+def createResultFilePath(filename):
+    expname = Path(filename).name.split('.')
+    expname = expname[0] + "_result.csv"
+    outfile = Path(filename).parent / expname
+    return outfile
 
 if __name__ == "__main__":
     # run(name_to_node_objs_list_map["a_b_a+b"])
@@ -81,10 +94,30 @@ if __name__ == "__main__":
     # csv_file_path_for_obj_demands_list = "csv/exp2_ec_9nodes_demand.csv"
 
     # csv_file_path_for_node_id_objs_list = "csv/exp3_rep_6nodes_placement.csv"
-    csv_file_path_for_node_id_objs_list = "csv/exp3_ec_6nodes_placement.csv"
-    csv_file_path_for_obj_demands_list = "csv/exp3_6nodes_demand.csv"
+    # csv_file_path_for_node_id_objs_list = "csv/exp3_ec_6nodes_placement.csv"
+    # csv_file_path_for_obj_demands_list = "csv/exp3_6nodes_demand.csv"
+
+    csv_file_path_for_node_id_objs_list_coding = "csv/SIMRESULT_SERVICE_RATE_CODING_PLACE_PLACEMENT.csv"
+    csv_file_path_for_obj_demands_list_coding = "csv/SIMRESULT_SERVICE_RATE_CODING_PLACE_DEMAND.csv"
     
     run_w_csv_file_path(
-        csv_file_path_for_node_id_objs_list,
-        csv_file_path_for_obj_demands_list
+        csv_file_path_for_node_id_objs_list_coding,
+        csv_file_path_for_obj_demands_list_coding
     )
+
+    csv_file_path_for_node_id_objs_list_replication = "csv/SIMRESULT_SERVICE_RATE_REPLICATION_PLACE_PLACEMENT.csv"
+    csv_file_path_for_obj_demands_list_replication = "csv/SIMRESULT_SERVICE_RATE_REPLICATION_PLACE_DEMAND.csv"
+
+    run_w_csv_file_path(
+        csv_file_path_for_node_id_objs_list_replication,
+        csv_file_path_for_obj_demands_list_replication
+    )
+
+    codingResFile = createResultFilePath(csv_file_path_for_obj_demands_list_coding)
+    repResFile = createResultFilePath(csv_file_path_for_obj_demands_list_replication)
+    df1 = pd.read_csv(codingResFile)
+    df2 = pd.read_csv(repResFile)
+    df = pd.concat([df1, df2])
+    df = df.sort_values(by=list(df.columns[:-3]))
+    df.reset_index(inplace=True, drop=True)
+    df.to_csv("csv/experiment_output.csv",index=False)
