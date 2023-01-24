93c3064fe5057091f48905df1d95150d55d6e73b
Mehmet Aktas <mfatihaktas@gmail.com>
2023-01-24 16:02:37 -0500

    Added srun.sh

---
+++ srun.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo $1 $2 $3
+
+if [ $1 = 'i' ]; then
+  srun --partition=main --nodes=1 --ntasks=1 --cpus-per-task=1 --mem=4000 --time=2:00:00 --export=ALL --pty bash -i
+  # srun --partition=main --nodes=1 --ntasks=1 --cpus-per-task=20 --mem=16000 --time=3:00:00 --export=ALL --pty bash -i
+else
+  echo "Did not match, arg= ${1}"
+fi
