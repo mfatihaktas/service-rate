1bfbe9ee1e2d89cb61cf7ec1cb1a6148da03c047
Mehmet Aktas <mfatihaktas@gmail.com>
2023-01-24 15:50:07 -0500

    Moved to test_w_integer_programming()

--- tests/test_integrality_theorem.py
+++ tests/test_integrality_theorem.py
@@ -6,33 +6,36 @@ from src import service_rate_utils
 from src.debug_utils import *
 
 
-def test_integrality_theorem():
-    # k, n = 2, 2
-    M = numpy.zeros((4, 4))
-    b = numpy.zeros((4, 1))
+def test_w_integer_programming():
+    k, n = 3, 3
+    # Obj choice constraints
+    obj_req_left_list, obj_req_right_list = [], []
+    obj_req_left_list.append([1, 1, 1, 0, 0, 0, 0, 0, 0])
+    obj_req_right_list.append([2])
+    obj_req_left_list.append([0, 0, 0, 1, 1, 1, 0, 0, 0])
+    obj_req_right_list.append([2])
+    obj_req_left_list.append([0, 0, 0, 0, 0, 0, 1, 1, 1])
+    obj_req_right_list.append([1])
 
-    # obj-0, 1
-    M[0, :] = [1, 1, 0, 0]
-    b[0] = 1
-    M[1, :] = [0, 0, 1, 1]
-    b[1] = 1
-    # node-0, 1
-    M[2, :] = [1, 0, 1, 0]
-    b[2] = 1
-    M[3, :] = [0, 1, 0, 1]
-    b[3] = 1
+    obj_req_left_matrix = numpy.array(obj_req_left_list)
+    obj_req_right_matrix = numpy.array(obj_req_right_list)
 
-    x = cvxpy.Variable(shape=(4, 1), name="x", integer=True)
+    # Node constraints
+    # node_req_left_list, node_req_right_list = [], []
+    # node_req_left_list.append([1, 0, 0, 1, 0, 0, 1, 0, 0])
+    # node_req_right_list.append([1])
+
+    x = cvxpy.Variable(shape=(k*n, 1), name="x", integer=True)
 
     obj = cvxpy.Minimize(cvxpy.sum(x))
     # obj = cvxpy.Minimize(cvxpy.norm(x, 1))
     # obj = cvxpy.Minimize(cvxpy.min(x) - cvxpy.max(x))
-    constraints = [M @ x == b, x >= 0]
+    constraints = [obj_req_left_matrix @ x >= obj_req_right_matrix, x >= 0, x <= 1]
 
     prob = cvxpy.Problem(obj, constraints)
     # opt_value = service_rate_utils.solve_prob(prob)
-    # prob.solve(solver="GLPK")
+    prob.solve(solver="GLPK")
     prob.solve()
-    check(prob.status == cvxpy.OPTIMAL, "")
+    # check(prob.status == cvxpy.OPTIMAL, "")
 
     log(DEBUG, "", prob_value=prob.value, x_value=x.value)
