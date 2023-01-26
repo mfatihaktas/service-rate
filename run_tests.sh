7b671ee43c5a275c1702045dca5013e122693e46
Mehmet Aktas <mfatihaktas@gmail.com>
2023-01-26 12:27:59 -0500

    Added option "i" in run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 
-if [ $1 = 's' ]; then
-  # pytest -rA -v tests/test_service_rate_inspector.py
-  # pytest -rA -v tests/test_service_rate_inspector.py -k "test_is_in_cap_region"
-  # pytest -rA -v tests/test_service_rate_inspector.py -k "test_plot_cap_2d"
-  # pytest -rA -v tests/test_service_rate_inspector.py -k "test_min_max_functions"
-  pytest -rA -v tests/test_service_rate_inspector.py -k "test_w_frac_of_demand_vectors_in_cap_region"
+PYTEST="pytest -rA -v --color=yes"
 
-elif [ $1 = 'x' ]; then
-  pytest -rA -v tests/test_service_rate_inspector_on_redundancy_w_two_xors.py
+if [ $1 = "s" ]; then
+  # ${PYTEST} tests/test_service_rate_inspector.py
+  # ${PYTEST} tests/test_service_rate_inspector.py -k "test_is_in_cap_region"
+  # ${PYTEST} tests/test_service_rate_inspector.py -k "test_plot_cap_2d"
+  # ${PYTEST} tests/test_service_rate_inspector.py -k "test_min_max_functions"
+   tests/test_service_rate_inspector.py -k "test_w_frac_of_demand_vectors_in_cap_region"
 
-elif [ $1 = 'c' ]; then
-  pytest -rA -v tests/test_service_rate_inspector_w_csv.py
+elif [ $1 = "x" ]; then
+  ${PYTEST} tests/test_service_rate_inspector_on_redundancy_w_two_xors.py
 
-elif [ $1 = 'l' ]; then
-  # pytest -rA -v tests/test_load_across_nodes.py
-  pytest -rA -v tests/test_load_across_nodes.py -k "test_load_across_nodes"
-  # pytest -rA -v tests/test_load_across_nodes.py -k "test_load_on_first_node"
+elif [ $1 = "c" ]; then
+  ${PYTEST} tests/test_service_rate_inspector_w_csv.py
+
+elif [ $1 = "l" ]; then
+  # ${PYTEST} tests/test_load_across_nodes.py
+  ${PYTEST} tests/test_load_across_nodes.py -k "test_load_across_nodes"
+  # ${PYTEST} tests/test_load_across_nodes.py -k "test_load_on_first_node"
+
+elif [ $1 = "i" ]; then
+  ${PYTEST} "tests/test_integrality_theorem.py::test_w_integer_programming_2"
 
 else
   echo "Unexpected arg= $1"
