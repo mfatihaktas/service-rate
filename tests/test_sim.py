93a109487e94bb03c1047c1d2c93c1a08d39b5aa
Mehmet Aktas <mfatihaktas@gmail.com>
2023-01-27 15:15:58 -0500

    Added tests/test_sim

---
+++ tests/test_sim.py
@@ -0,0 +1,40 @@
+import pytest
+import simpy
+
+from src.sim import (
+    random_variable,
+    sim as sim_module,
+)
+from src.debug_utils import *
+
+
+@pytest.fixture(scope="module")
+def env() -> simpy.Environment:
+    return simpy.Environment()
+
+
+@pytest.fixture(scope="module")
+def inter_gen_time_rv() -> random_variable.RandomVariable:
+    return random_variable.Exponential(mu=1)
+
+
+@pytest.fixture(scope="module")
+def service_time_rv() -> random_variable.RandomVariable:
+    return random_variable.Exponential(mu=1)
+
+
+def test_MM1(
+    env: simpy.Environment,
+    inter_gen_time_rv: random_variable.RandomVariable,
+    service_time_rv: random_variable.RandomVariable,
+):
+    num_sim_runs = 1
+    num_requests_to_serve = 10
+    sim_result = sim_module.sim_single_server_w_joblib(
+        env=env,
+        inter_gen_time_rv=inter_gen_time_rv,
+        service_time_rv=service_time_rv,
+        num_requests_to_serve=num_requests_to_serve,
+        num_sim_runs=num_sim_runs,
+    )
+    log(INFO, "", sim_result=sim_result)
