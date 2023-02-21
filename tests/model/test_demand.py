4c336fb05655186e089464b27d84a4a5dc9cd1ec
Mehmet Fatih Aktas <mfatihaktas@gmail.com>
2023-02-21 12:59:31 -0500

    Added tests/model/test_demand

---
+++ tests/model/test_demand.py
@@ -0,0 +1,14 @@
+from src.model import demand
+
+from src.utils.debug import *
+
+
+def test_gen_demand_vector_w_zipf_law():
+    for demand_vector in demand.sample_demand_vectors_w_zipf_law(
+        num_obj=3,
+        num_popular_obj=2,
+        cum_demand=2,
+        zipf_tail_index=0,
+        num_sample=5,
+    ):
+        log(DEBUG, "", demand_vector=demand_vector)
