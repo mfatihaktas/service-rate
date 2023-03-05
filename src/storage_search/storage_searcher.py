d20c31039f557a3a50a1ab008a8e4e130f2362f1
Mehmet Fatih Aktas <mfatihaktas@gmail.com>
2023-03-05 00:34:57 -0500

    Minor fix in src/storage_search/storage_searcher

--- src/storage_search/storage_searcher.py
+++ src/storage_search/storage_searcher.py
@@ -49,6 +49,8 @@ class StorageSearcher:
             in_cap_region_, min_distance_ = service_rate_inspector.get_in_cap_region_and_min_distance_to_boundary_w_cvxpy(
                 obj_demand_list=demand_vector,
             )
+            if in_cap_region_:
+                min_distance_ = 0
 
             # log(DEBUG, "",
             #     node_id_to_objs_list=node_id_to_objs_list,
