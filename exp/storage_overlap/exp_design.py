b3441a3afc5660aab9abdce8a6f298817c6258ca
Mehmet Fatih Aktas <mfatihaktas@gmail.com>
2023-03-11 02:16:31 -0500

    Set d=d in plot_frac_demand_vectors_covered_vs_num_popular_objs

--- exp/storage_overlap/exp_design.py
+++ exp/storage_overlap/exp_design.py
@@ -227,12 +227,12 @@ def plot_frac_demand_vectors_covered_vs_num_popular_objs(
     use_cvxpy = False
     replica_design_list = [
         design.ClusteringDesign(k=k, n=n, d=d, use_cvxpy=use_cvxpy),
-        design_w_stripe.ClusteringDesignWithStripe(k=k, n=n, d=(2 * d), s=2, use_cvxpy=use_cvxpy),
+        design_w_stripe.ClusteringDesignWithStripe(k=k, n=n, d=d, s=2, use_cvxpy=use_cvxpy),
         # design.CyclicDesign(k=k, n=n, d=d, shift_size=1, use_cvxpy=use_cvxpy),
         # design.CyclicDesign(k=k, n=n, d=d, shift_size=2, use_cvxpy=use_cvxpy),
         # design.CyclicDesign(k=k, n=n, d=d, shift_size=3, use_cvxpy=use_cvxpy),
         design.RandomBlockDesign(k=k, n=n, d=d, use_cvxpy=use_cvxpy),
-        design_w_stripe.RandomBlockDesignWithStripe(k=k, n=n, d=(2 * d), s=2, use_cvxpy=use_cvxpy),
+        design_w_stripe.RandomBlockDesignWithStripe(k=k, n=n, d=d, s=2, use_cvxpy=use_cvxpy),
         # design.RandomExpanderDesign(k=k, n=n, d=d, use_cvxpy=use_cvxpy),
         # design.RandomExpanderDesign_wClusters(k=k, n=n, d=d, num_clusters=2, use_cvxpy=use_cvxpy),
         # design.TwoXORDesign(k=124, n=124, d=d, use_cvxpy=use_cvxpy),
