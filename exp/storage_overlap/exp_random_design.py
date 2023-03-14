83fe3cf0f99d53dc5919ead2cd2e87fd861ffd09
Mehmet Fatih Aktas <mfatihaktas@gmail.com>
2023-03-13 23:34:36 -0400

    Refactored exp/storage_overlap/exp_random_design

--- exp/storage_overlap/exp_random_design.py
+++ exp/storage_overlap/exp_random_design.py
@@ -10,6 +10,82 @@ from src.utils.debug import *
 from src.utils.plot import *
 
 
+COMBINATION_SIZE_TO_COLOR_MAP = {}
+
+
+def plot_frac_demand_vectors_covered_for_given_combination_size_vs_num_popular_objs_for_storage_design(
+    storage_design: design.StorageDesign,
+    demand_for_popular: int,
+    num_popular_obj_list: list[int],
+    combination_size: int,
+    num_sample: int = 300,
+    num_sim_run: int = 3,
+):
+    # nonlocal COMBINATION_SIZE_TO_COLOR_MAP
+
+    log(INFO, "Started",
+        storage_design=storage_design,
+        demand_for_popular=demand_for_popular,
+        num_popular_obj_list=num_popular_obj_list,
+        combination_size=combination_size,
+        num_sample=num_sample,
+        num_sim_run=num_sim_run,
+    )
+
+    if combination_size not in COMBINATION_SIZE_TO_COLOR_MAP:
+        COMBINATION_SIZE_TO_COLOR_MAP[combination_size] = next(dark_color_cycle)
+
+    E_frac_of_demand_vectors_covered_list = []
+    std_frac_of_demand_vectors_covered_list = []
+
+    for num_popular_obj in num_popular_obj_list:
+        log(INFO, f"> num_popular_obj= {num_popular_obj}")
+
+        if (
+            len(E_frac_of_demand_vectors_covered_list) > 0
+            and E_frac_of_demand_vectors_covered_list[-1] <= 0.01
+        ):
+            frac_of_demand_vectors_covered_list = [0]
+
+        else:
+            frac_of_demand_vectors_covered_list = sim.sim_frac_of_demand_vectors_covered(
+                storage_design=storage_design,
+                num_popular_obj=num_popular_obj,
+                cum_demand=demand_for_popular * num_popular_obj,
+                zipf_tail_index=0,
+                num_sample=num_sample,
+                num_sim_run=num_sim_run,
+                combination_size_for_is_demand_vector_covered=combination_size,
+            )
+        E_frac_of_demand_vectors_covered_list.append(numpy.mean(frac_of_demand_vectors_covered_list))
+        std_frac_of_demand_vectors_covered_list.append(numpy.std(frac_of_demand_vectors_covered_list))
+
+    log(INFO, "",
+        storage_design=storage_design,
+        demand_for_popular=demand_for_popular,
+        num_popular_obj_list=num_popular_obj_list,
+        E_frac_of_demand_vectors_covered_list=E_frac_of_demand_vectors_covered_list,
+        std_frac_of_demand_vectors_covered_list=std_frac_of_demand_vectors_covered_list,
+    )
+
+    plot.errorbar(num_popular_obj_list, E_frac_of_demand_vectors_covered_list, yerr=std_frac_of_demand_vectors_covered_list, label=f"{storage_design.repr_for_plot()}, C={combination_size}", color=COMBINATION_SIZE_TO_COLOR_MAP[combination_size], marker=next(marker_cycle), linestyle="dotted", lw=2, mew=3, ms=5)
+
+    fontsize = 14
+    plot.legend(fontsize=fontsize)
+    # plot.yscale("log")
+    plot.ylabel("Fraction of demand vectors covered", fontsize=fontsize)
+    plot.xlabel("Number of popular objects", fontsize=fontsize)
+
+    plot.title(
+        fr"$d= {storage_design.d}$, "
+        r"$\lambda= $" + fr"${demand_for_popular}$, "
+        r"$N_{\textrm{sample}}= $" + fr"${num_sample}$, "
+        r"$N_{\textrm{sim}}= $" + fr"${num_sim_run}$"
+    )
+
+    log(INFO, "Done")
+
+
 def plot_frac_demand_vectors_covered_for_given_combination_size_vs_num_popular_objs(
     d: int,
     demand_for_popular: int,
@@ -19,141 +95,35 @@ def plot_frac_demand_vectors_covered_for_given_combination_size_vs_num_popular_o
     k = 120
     n = k
     use_cvxpy = True
-    block_design = design.RandomBlockDesign(k=k, n=n, d=d, use_cvxpy=use_cvxpy)
-    random_expander_design = design.RandomExpanderDesign(k=k, n=n, d=d, use_cvxpy=use_cvxpy)
-    random_expander_design_w_clusters = design.RandomExpanderDesign_wClusters(
-        k=k, n=n, d=d, use_cvxpy=use_cvxpy, num_clusters=2,
-    )
 
     # num_popular_obj_list = [2, 5, 10] + [int(k * frac) for frac in [0.1, 0.15, 0.2, 0.25]]
     # num_popular_obj_list = [2, 5, 10] + [int(k * frac) for frac in [0.1, 0.6, 0.8]]
-
     num_popular_obj_list = [2, 5, 10, 20]
+    # num_popular_obj_list = [2, 5]
 
     log(INFO, "Started",
-        block_design=block_design,
-        random_expander_design=random_expander_design,
-        random_expander_design_w_clusters=random_expander_design_w_clusters,
         num_popular_obj_list=num_popular_obj_list,
         demand_for_popular=demand_for_popular,
         num_sample=num_sample,
         num_sim_run=num_sim_run,
     )
 
-    def plot_(combination_size: int):
-        log(INFO, f">> combination_size= {combination_size}")
+    storage_design_list = [
+        # design.RandomBlockDesign(k=k, n=n, d=d, use_cvxpy=use_cvxpy),
+        design.RandomExpanderDesign(k=k, n=n, d=d, use_cvxpy=use_cvxpy),
+        # design.RandomExpanderDesign_wClusters(k=k, n=n, d=d, use_cvxpy=use_cvxpy, num_clusters=2),
+    ]
 
-        E_frac_of_demand_vectors_covered_list_for_block_design = []
-        std_frac_of_demand_vectors_covered_list_for_block_design = []
-        E_frac_of_demand_vectors_covered_list_for_expander_design = []
-        std_frac_of_demand_vectors_covered_list_for_expander_design = []
-        E_frac_of_demand_vectors_covered_list_for_expander_design_w_clusters = []
-        std_frac_of_demand_vectors_covered_list_for_expander_design_w_clusters = []
-        # frac_of_demand_vectors_covered_lower_bound_list = []
-        # frac_of_demand_vectors_covered_upper_bound_list = []
-
-        for num_popular_obj in num_popular_obj_list:
-            log(INFO, f"> num_popular_obj= {num_popular_obj}")
-
-            # Run sim for `block_design`
-            frac_of_demand_vectors_covered_list_for_block_design = [0]
-            if (
-                len(E_frac_of_demand_vectors_covered_list_for_block_design) == 0
-                or E_frac_of_demand_vectors_covered_list_for_block_design[-1] > 0.01
-            ):
-                frac_of_demand_vectors_covered_list_for_block_design = sim.sim_frac_of_demand_vectors_covered(
-                    storage_design=block_design,
-                    num_popular_obj=num_popular_obj,
-                    cum_demand=demand_for_popular * num_popular_obj,
-                    zipf_tail_index=0,
-                    num_sample=num_sample,
-                    num_sim_run=num_sim_run,
-                    combination_size_for_is_demand_vector_covered=combination_size,
-                )
-            E_frac_of_demand_vectors_covered_list_for_block_design.append(numpy.mean(frac_of_demand_vectors_covered_list_for_block_design))
-            std_frac_of_demand_vectors_covered_list_for_block_design.append(numpy.std(frac_of_demand_vectors_covered_list_for_block_design))
-
-            # Run sim for `random_expander_design`
-            frac_of_demand_vectors_covered_list_for_expander_design = [0]
-            if (
-                len(E_frac_of_demand_vectors_covered_list_for_expander_design) == 0
-                or E_frac_of_demand_vectors_covered_list_for_expander_design[-1] > 0.01
-            ):
-                frac_of_demand_vectors_covered_list_for_expander_design = sim.sim_frac_of_demand_vectors_covered(
-                    storage_design=random_expander_design,
-                    num_popular_obj=num_popular_obj,
-                    cum_demand=demand_for_popular * num_popular_obj,
-                    zipf_tail_index=0,
-                    num_sample=num_sample,
-                    num_sim_run=num_sim_run,
-                    combination_size_for_is_demand_vector_covered=combination_size,
-                )
-            E_frac_of_demand_vectors_covered_list_for_expander_design.append(numpy.mean(frac_of_demand_vectors_covered_list_for_expander_design))
-            std_frac_of_demand_vectors_covered_list_for_expander_design.append(numpy.std(frac_of_demand_vectors_covered_list_for_expander_design))
-
-            # Run sim for `random_expander_design_w_clusters`
-            frac_of_demand_vectors_covered_list_for_expander_design_w_clusters = [0]
-            if (
-                len(E_frac_of_demand_vectors_covered_list_for_expander_design_w_clusters) == 0
-                or E_frac_of_demand_vectors_covered_list_for_expander_design_w_clusters[-1] > 0.01
-            ):
-                frac_of_demand_vectors_covered_list_for_expander_design_w_clusters = sim.sim_frac_of_demand_vectors_covered(
-                    storage_design=random_expander_design_w_clusters,
-                    num_popular_obj=num_popular_obj,
-                    cum_demand=demand_for_popular * num_popular_obj,
-                    zipf_tail_index=0,
-                    num_sample=num_sample,
-                    num_sim_run=num_sim_run,
-                    combination_size_for_is_demand_vector_covered=combination_size,
-                )
-            E_frac_of_demand_vectors_covered_list_for_expander_design_w_clusters.append(numpy.mean(frac_of_demand_vectors_covered_list_for_expander_design_w_clusters))
-            std_frac_of_demand_vectors_covered_list_for_expander_design_w_clusters.append(numpy.std(frac_of_demand_vectors_covered_list_for_expander_design_w_clusters))
-
-            # frac_of_demand_vectors_covered_lower_bound, frac_of_demand_vectors_covered_upper_bound = sim.sim_frac_of_demand_vectors_covered_lower_and_upper_bound(
-            #     storage_design=replica_design,
-            #     num_popular_obj=num_popular_obj,
-            #     cum_demand=demand_for_popular * num_popular_obj,
-            #     zipf_tail_index=0,
-            #     num_sample=num_sample,
-            #     num_sim_run=num_sim_run,
-            # )
-            # frac_of_demand_vectors_covered_lower_bound_list.append(frac_of_demand_vectors_covered_lower_bound)
-            # frac_of_demand_vectors_covered_upper_bound_list.append(frac_of_demand_vectors_covered_upper_bound)
-
-        log(INFO, "",
-            d=d,
-            demand_for_popular=demand_for_popular,
-            num_popular_obj_list=num_popular_obj_list,
-            E_frac_of_demand_vectors_covered_list_for_block_design=E_frac_of_demand_vectors_covered_list_for_block_design,
-            std_frac_of_demand_vectors_covered_list_for_block_design=std_frac_of_demand_vectors_covered_list_for_block_design,
-            E_frac_of_demand_vectors_covered_list_for_expander_design=E_frac_of_demand_vectors_covered_list_for_expander_design,
-            std_frac_of_demand_vectors_covered_list_for_expander_design=std_frac_of_demand_vectors_covered_list_for_expander_design,
-            E_frac_of_demand_vectors_covered_list_for_expander_design_w_clusters=E_frac_of_demand_vectors_covered_list_for_expander_design_w_clusters,
-            std_frac_of_demand_vectors_covered_list_for_expander_design_w_clusters=std_frac_of_demand_vectors_covered_list_for_expander_design_w_clusters,
-        )
-
-        color = next(dark_color_cycle)
-        plot.errorbar(num_popular_obj_list, E_frac_of_demand_vectors_covered_list_for_block_design, yerr=std_frac_of_demand_vectors_covered_list_for_block_design, label=f"{block_design.repr_for_plot()}, C={combination_size}", color=color, marker=next(marker_cycle), linestyle="dotted", lw=2, mew=3, ms=5)
-        plot.errorbar(num_popular_obj_list, E_frac_of_demand_vectors_covered_list_for_expander_design, yerr=std_frac_of_demand_vectors_covered_list_for_expander_design, label=f"{random_expander_design.repr_for_plot()}, C={combination_size}", color=color, marker=next(marker_cycle), linestyle="dotted", lw=2, mew=3, ms=5)
-        plot.errorbar(num_popular_obj_list, E_frac_of_demand_vectors_covered_list_for_expander_design_w_clusters, yerr=std_frac_of_demand_vectors_covered_list_for_expander_design, label=f"{random_expander_design_w_clusters.repr_for_plot()}, C={combination_size}", color=color, marker=next(marker_cycle), linestyle="dotted", lw=2, mew=3, ms=5)
-        # plot.plot(num_popular_obj_list, frac_of_demand_vectors_covered_lower_bound_list, label=f"{replica_design.repr_for_plot()}-LB", color=color, marker=next(marker_cycle), linestyle="dotted", lw=2, mew=3, ms=5)
-        # plot.plot(num_popular_obj_list, frac_of_demand_vectors_covered_upper_bound_list, label=f"{replica_design.repr_for_plot()}-UB", color=color, marker=next(marker_cycle), linestyle="dotted", lw=2, mew=3, ms=5)
-
-    for combination_size in range(2, d + 1):
-        plot_(combination_size=combination_size)
-
-    fontsize = 14
-    plot.legend(fontsize=fontsize)
-    # plot.yscale("log")
-    plot.ylabel("Fraction of demand vectors covered", fontsize=fontsize)
-    plot.xlabel("Number of popular objects", fontsize=fontsize)
-
-    plot.title(
-        fr"$d= {d}$, "
-        r"$\lambda= $" + fr"${demand_for_popular}$, "
-        r"$N_{\textrm{sample}}= $" + fr"${num_sample}$, "
-        r"$N_{\textrm{sim}}= $" + fr"${num_sim_run}$"
-    )
+    for storage_design in storage_design_list:
+        for combination_size in range(2, d + 1):
+            plot_frac_demand_vectors_covered_for_given_combination_size_vs_num_popular_objs_for_storage_design(
+                storage_design=storage_design,
+                demand_for_popular=demand_for_popular,
+                num_popular_obj_list=num_popular_obj_list,
+                combination_size=combination_size,
+                num_sample=num_sample,
+                num_sim_run=num_sim_run,
+            )
 
     # Save the plot
     plot.gcf().set_size_inches(8, 6)
@@ -215,5 +185,5 @@ def manage_plot_frac_demand_vectors_covered_for_given_combination_size_vs_num_po
 
 
 if __name__ == "__main__":
-    manage_plot_frac_demand_vectors_covered_for_given_combination_size_vs_num_popular_objs()
-    # manage_plot_frac_demand_vectors_covered_for_given_combination_size_vs_num_popular_objs_w_joblib()
+    # manage_plot_frac_demand_vectors_covered_for_given_combination_size_vs_num_popular_objs()
+    manage_plot_frac_demand_vectors_covered_for_given_combination_size_vs_num_popular_objs_w_joblib()
