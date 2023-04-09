21006a82e21083d0196eaf712e29e370fd31b7fa
Mehmet Fatih Aktas <mfatihaktas@gmail.com>
2023-04-08 21:11:50 -0400

    Added exp/storage_overlap/exp_impact_of_b and src/storage_overlap/model

---
+++ exp/storage_overlap/exp_impact_of_b.py
@@ -0,0 +1,154 @@
+import joblib
+import numpy
+
+from src.model import (
+    demand,
+)
+from src.storage_overlap import (
+    design,
+    model,
+    sim,
+)
+
+from src.utils.plot import *
+
+
+def plot_frac_demand_vectors_covered_vs_b(
+    b_max: int,
+    demand_b_1: int,
+    num_samples: int = 300,
+    num_sim_run: int = 3,
+):
+    # k = 45
+    # k = 111
+    # k = 120
+    k = 24
+    n = k
+
+    log(INFO, "Started",
+        b_max=b_max,
+        demand_b_1=demand_b_1,
+        num_samples=num_samples,
+        num_sim_run=num_sim_run,
+    )
+
+    def plot_(
+        b: int,
+        storage_design: design.ReplicaDesign,
+        storage_model: model.NoRedundancyDesignModel,
+    ):
+        log(INFO, f">> b= {b}, storage_design= {storage_design}")
+
+        prob_obj_is_active_list = []
+
+        E_frac_of_demand_vectors_covered_list = []
+        std_frac_of_demand_vectors_covered_list = []
+        prob_serving_model_list = []
+
+        for prob_obj_is_active in numpy.linspace(0.1, 0.8, 8):
+            log(INFO, f"> prob_obj_is_active= {prob_obj_is_active}")
+
+            prob_obj_is_active_list.append(prob_obj_is_active)
+
+            # frac_of_demand_vectors_covered_list = [0.02, 0.02]
+            # sample_demand_vector = lambda: demand.sample_demand_vector_w_p(
+            #     num_objs=storage_design.k,
+            #     demand_for_active_obj=demand_b_1,
+            #     prob_obj_is_active=prob_obj_is_active,
+            # )
+
+            frac_of_demand_vectors_covered_list = [0, 0]
+            # frac_of_demand_vectors_covered_list = sim.sim_frac_of_demand_vectors_covered(
+            #     sample_demand_vector=sample_demand_vector,
+            #     storage_design=storage_design,
+            #     num_samples=num_samples,
+            #     num_sim_run=num_sim_run,
+            # )
+
+            E_frac_of_demand_vectors_covered = numpy.mean(frac_of_demand_vectors_covered_list)
+            E_frac_of_demand_vectors_covered_list.append(E_frac_of_demand_vectors_covered)
+            std_frac_of_demand_vectors_covered_list.append(numpy.std(frac_of_demand_vectors_covered_list))
+
+            prob_serving_model = storage_model.prob_serving_downscaling_demand_per_obj(p=prob_obj_is_active, lambda_b_1=demand_b_1)
+            # prob_serving_model = storage_model.prob_serving_downscaling_p_per_obj(p=prob_obj_is_active, lambda_b_1=demand_b_1)
+            prob_serving_model_list.append(prob_serving_model)
+
+            if prob_serving_model < 0.01:
+                break
+
+        log(INFO, f"storage_design= {storage_design}",
+            prob_obj_is_active_list=prob_obj_is_active_list,
+            E_frac_of_demand_vectors_covered_list=E_frac_of_demand_vectors_covered_list,
+            std_frac_of_demand_vectors_covered_list=std_frac_of_demand_vectors_covered_list,
+        )
+
+        color = next(dark_color_cycle)
+        # plot.errorbar(prob_obj_is_active_list, E_frac_of_demand_vectors_covered_list, yerr=std_frac_of_demand_vectors_covered_list, label=f"b={b}", color=color, marker=next(marker_cycle), linestyle="dotted", lw=2, mew=3, ms=5)
+        plot.plot(prob_obj_is_active_list, prob_serving_model_list, label=f"b={b}, Model", color=color, marker=next(marker_cycle), linestyle="dotted", lw=2, mew=3, ms=5)
+
+    use_cvxpy = True  # False
+    b_storage_design_model_list = [
+        (
+            b,
+            design.NoRedundancyDesign(k=b * k, n=n, use_cvxpy=use_cvxpy),
+            model.NoRedundancyDesignModel(k=k, n=n, b=b)
+        )
+        for b in range(2, b_max + 1)
+    ]
+
+    for b, storage_design, storage_model in b_storage_design_model_list:
+        plot_(b=b, storage_design=storage_design, storage_model=storage_model)
+
+    fontsize = 14
+    plot.legend(fontsize=fontsize)
+    # plot.yscale("log")
+    plot.ylabel(r"$\mathcal{P}_{p, \lambda}$", fontsize=fontsize)
+    plot.xlabel(r"$p$", fontsize=fontsize)
+
+    plot.title(
+        r"$\lambda_{\textrm{max-per-node}}= $" + fr"${demand_b_1}$, "
+        r"$N_{\textrm{sample}}= $" + fr"${num_samples}$, "
+        r"$N_{\textrm{sim}}= $" + fr"${num_sim_run}$"
+    )
+
+    # Save the plot
+    plot.gcf().set_size_inches(8, 6)
+    file_name = (
+        "plots/plot_frac_demand_vectors_covered_vs_b"
+        + f"_k_{k}"
+        + f"_b_max_{b_max}"
+        + f"_lambda_{demand_b_1}"
+        + ".png"
+    )
+    plot.savefig(file_name, bbox_inches="tight")
+    plot.gcf().clear()
+
+    log(INFO, "Done")
+
+
+def manage_plot_frac_demand_vectors_covered_vs_b_w_joblib():
+    log(INFO, "Started")
+
+    joblib.Parallel(n_jobs=-1, prefer="processes")(
+        joblib.delayed(plot_frac_demand_vectors_covered_vs_b)(
+            b_max=b_max,
+            demand_b_1=demand_b_1,
+            num_samples=300,
+            # num_samples=1000,
+            num_sim_run=3,
+        )
+        # for b_max in [4]
+        for b_max in [10]
+        # for demand_b_1 in numpy.arange(0.2, 1.0, 0.1)
+        for demand_b_1 in numpy.arange(1, 2, 0.1)
+        # for demand_b_1 in [0.6]
+        # for demand_b_1 in [1.1]
+        # for demand_b_1 in [1.5]
+        # for demand_b_1 in [1.5, 2, 2.5, 3]
+    )
+
+    log(INFO, "Done")
+
+
+if __name__ == "__main__":
+    manage_plot_frac_demand_vectors_covered_vs_b_w_joblib()
---
+++ src/storage_overlap/model.py
@@ -0,0 +1,159 @@
+import abc
+import dataclasses
+import math
+import scipy.stats
+
+from src.allocation_w_complexes import model as allocation_w_complexes_model
+
+from src.utils.debug import *
+
+
+@dataclasses.dataclass
+class StorageDesignModel:
+    k: int
+    n: int
+
+
+@dataclasses.dataclass
+class NoRedundancyDesignModel(StorageDesignModel):
+    b: int
+
+    def prob_serving_downscaling_demand_per_obj(self, p: int, lambda_b_1: float) -> float:
+        lambda_ = lambda_b_1 / self.b
+        num_active_objs_handled_by_node = math.floor(1 / lambda_)
+
+        prob_sys_can_serve = 1
+
+        for node_id in range(self.n):
+            prob_node_can_serve = scipy.stats.binom.cdf(num_active_objs_handled_by_node, self.b, p)
+            prob_sys_can_serve *= prob_node_can_serve
+
+        return prob_sys_can_serve
+
+    def prob_serving_downscaling_p_per_obj(self, p: int, lambda_b_1: float) -> float:
+        num_active_objs_handled_by_node = math.floor(1 / lambda_b_1)
+        p_ = p / self.b
+
+        prob_sys_can_serve = 1
+
+        for node_id in range(self.n):
+            prob_node_can_serve = scipy.stats.binom.cdf(num_active_objs_handled_by_node, self.b, p_)
+            prob_sys_can_serve *= prob_node_can_serve
+
+        return prob_sys_can_serve
+
+@dataclasses.dataclass
+class ReplicaDesignModel:
+    d: int
+
+    @abc.abstractmethod
+    def prob_span_is_larger_than_m_times_lambda(
+        self,
+        m: int,
+        lambda_: float,
+    ):
+        pass
+
+    @abc.abstractmethod
+    def prob_serving_upper_bound(self, m: int, lambda_: int):
+        pass
+
+    @abc.abstractmethod
+    def prob_serving_lower_bound(self, m: int, lambda_: int):
+        pass
+
+
+@dataclasses.dataclass
+class RandomExpanderDesignModel(ReplicaDesignModel):
+    def prob_span_is_larger_than_m_times_lambda(
+        self,
+        m: int,
+        lambda_: float,
+    ) -> float:
+        min_span = math.ceil(m * lambda_)
+        return allocation_w_complexes_model.prob_num_nonempty_cells_geq_c(
+            n=self.n, m=m, d=self.d, c=min_span
+        )
+
+    def prob_serving(self, m: int, lambda_: int) -> float:
+        return min(
+            self.prob_span_is_larger_than_m_times_lambda(
+                m=m_,
+                lambda_=lambda_,
+            )
+            for m_ in range(1, m + 1)
+        )
+
+    def prob_serving_upper_bound(self, m: int, lambda_: int) -> float:
+        return min(
+            self.prob_span_is_larger_than_m_times_lambda(
+                m=m_,
+                lambda_=lambda_,
+            )
+            for m_ in range(1, m + 1)
+        )
+
+    def prob_serving_upper_bound_(self, m: int, lambda_: int) -> float:
+        return min(
+            allocation_w_complexes_model.prob_span_of_every_t_complexes_geq_u_upper_bound(
+                n=self.n, m=m, d=self.d, t=m_, u=math.ceil(m_ * lambda_)
+            )
+            for m_ in range(1, m + 1)
+        )
+
+    def wrong_prob_serving_lower_bound(self, m: int, lambda_: int) -> float:
+        return math.prod(
+            [
+                self.prob_span_is_larger_than_m_times_lambda(
+                    m=m_,
+                    lambda_=lambda_,
+                )
+                for m_ in range(1, m + 1)
+            ]
+        )
+
+    def _prob_serving_lower_bound(self, m: int, lambda_: int) -> float:
+        # return allocation_w_complexes_model.prob_expand_span_by_e_with_each_complex(
+        #     n=self.n, m=m, d=self.d, e=lambda_
+        # )
+
+        # return allocation_w_complexes_model.prob_expand_span_by_e_with_each_complex(
+        #     n=self.n, m=m, d=self.d, e=1
+        # )
+
+        # return allocation_w_complexes_model.prob_expand_span_by_at_least_e_with_each_complex(
+        #     n=self.n, m=m, d=self.d, e=lambda_ - 1
+        # )
+
+        return allocation_w_complexes_model.prob_expand_span_as_necessary_faster(
+            n=self.n, m=m, d=self.d, lambda_=lambda_
+        )
+
+    def prob_serving_lower_bound(self, m: int, lambda_: int) -> float:
+        # return max(
+        #     allocation_w_complexes_model.prob_span_of_every_t_complexes_geq_u_lower_bound(
+        #         n=self.n, m=m, d=self.d, t=m_, u=math.ceil(m_ * lambda_)
+        #     )
+        #     for m_ in range(1, m + 1)
+        # )
+
+        return math.prod(
+            [
+                allocation_w_complexes_model.prob_span_of_every_t_complexes_geq_u_alternative(
+                    n=self.n, m=m, d=self.d, t=m_, u=math.ceil(m_ * lambda_)
+                )
+                for m_ in range(1, m + 1)
+            ]
+        )
+
+
+@dataclasses.dataclass
+class ClusteringDesignModel(ReplicaDesignModel):
+    def prob_serving_for_balls_into_bins_upper_bound(self, m: int, lambda_: int) -> float:
+        num_bins = self.n / self.d
+        num_balls = m
+        max_num_balls = math.ceil(self.d / lambda_)
+
+        prob_single_node_is_stable = scipy.stats.binom.cdf(max_num_balls, num_balls, 1 / num_bins)
+
+        return prob_single_node_is_stable**num_bins
